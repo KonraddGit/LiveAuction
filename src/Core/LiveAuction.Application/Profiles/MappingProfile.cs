07bb4d91728c67e12aa6042b34881a9c2bc7842d
KonraddGit <konradparchanski@gmail.com>
2023-03-27 13:46:37 +0200

    Add profile from AutoMapper function

---
+++ src/Core/LiveAuction.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LiveAuction.Application.Features.Auctions;
+using LiveAuction.Domain.Entities;
+
+namespace LiveAuction.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Auction, AuctionListVm>().ReverseMap();
+    }
+}
